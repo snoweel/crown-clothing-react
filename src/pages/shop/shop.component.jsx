4726cf65b8c2527b4adcfd1d98ef1c7b5fcbbe03
Snoweel <snoweel.pereira@intellect.design>
2021-06-28 21:58:11 +0530

    refactor collections to collections component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,14 +6,11 @@ import {connect} from "react-redux";
 import {createStructuredSelector} from "reselect";
 
 import {selectCollections} from "../../redux/shop/shop.selector";
-
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 const  ShopPage = ({collections}) => (
     <div className='shop-page'>
-        {
-            collections.map(({id,...otherCollectionProps}) =>(
-                <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-            ))
-        }
+        <CollectionsOverview/>
+
     </div>
 );
 
@@ -37,9 +34,10 @@ const  ShopPage = ({collections}) => (
 //         </div>)
 //     }
 // }
-const mapStateToProps =  createStructuredSelector({
-    collections: selectCollections
-    }
-)
+// const mapStateToProps =  createStructuredSelector({
+//     collections: selectCollections
+//     }
+// )
 
-export default connect(mapStateToProps)(ShopPage);
+// export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
