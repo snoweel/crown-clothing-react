41f0f0a01f37b4700312bc14a459774981b47185
Snoweel <snoweel.pereira@intellect.design>
2021-06-29 10:00:55 +0530

    dynamic route page working

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -10,12 +10,19 @@ import './collection.styles.scss'
 
 
 const CollectionPage = ({collection}) => {
-    // console.dir(collection);
+    console.dir(collection);
     // console.log(match.params.collectionId);
-
+    const {title,items} = collection;
     return (
-        <div className='category'>
-            <h2> COllection page</h2>
+        <div className='collection-page'>
+            <h2 className='title'> {title}</h2>
+            <div className='items'>
+                {
+                    items.map( item =>
+                        <CollectionItem key={item.id} item={item} />
+                    )
+                }
+            </div>
         </div>
     )
 }
